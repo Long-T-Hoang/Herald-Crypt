9fbe152e1c0169e3a8e064e81e072e73a2f7b9d5
Long Hoang <heroprotunlong@gmail.com>
2021-02-28 22:16:22 -0500

    Added movement and attack
    - Added player and projectile prefab
    - Added and implemented player attack and movement, and projectile movement

---
+++ Herald-Crypt/Assets/Scripts/PlayerScripts/PlayerAttack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Attack on mouse left click
+        if(Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = (mousePos - transform.position).normalized;
+        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, direction);
+
+        Instantiate(projectile, transform.position, rotation);
+    }
+}
---
+++ Herald-Crypt/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float newY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        Vector3 newPos = transform.position;
+
+        newPos.x += newX;
+        newPos.y += newY;
+
+        transform.position = newPos;
+    }
+}
