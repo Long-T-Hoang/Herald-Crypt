2c34a2573ea738decc5b0940913ef8fd5ba9e1f9
Long Hoang <heroprotunlong@gmail.com>
2021-03-02 12:40:40 -0500

    Added enemy prefab
    Working on enemy basic follow AI

---
+++ Herald-Crypt/Assets/Scripts/EnemyBehavior1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior1 : MonoBehaviour
+{
+    [SerializeField]
+    private float detectionRange;
+    [SerializeField]
+    private LayerMask playerMask;
+
+    private Collider2D playerCollider;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCollider = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerCollider = Physics2D.OverlapCircle(transform.position, detectionRange, playerMask.value);
+        
+        if (playerCollider != null)
+        {
+            player = playerCollider.gameObject;
+        }
+    }
+
+    void RotateToPlayer()
+    {
+
+    }
+
+    void MoveTo()
+    {
+
+    }
+}
