c80b0844b790ccc01c083ff2f360011711ac582c
Long Hoang <heroprotunlong@gmail.com>
2021-03-04 15:01:35 -0500

    Pathfinding
    -Added basic pathfinding script
    -Have yet to implement into enemy AI

--- Herald-Crypt/Assets/Scripts/Grid.cs
+++ Herald-Crypt/Assets/Scripts/Grid.cs
@@ -1,49 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid
+public class Grid<GridObject>
 {
     private int width, height;
     private float cellSize;
-    private int[,] gridArray;
+    private Vector3 startPoint;
+
+    private GridObject[,] gridArray;
     private TextMesh[,] debugTextArray;
+    private int[,] gridDebugArray;
+
+    // Properties
+    public int Width
+    {
+        get { return width; }
+    }
+
+    public int Height
+    {
+        get { return height; }
+    }
 
     // Constructor
-    public Grid(int width, int height, float cellSize)
+    public Grid(int width, int height, float cellSize, Vector3 startPoint, Func<Grid<GridObject>, int, int, GridObject> createGridObject = null)
     {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
+        this.startPoint = startPoint;
 
-        gridArray = new int[width, height];
+        gridArray = new GridObject[width, height];
         debugTextArray = new TextMesh[width, height];
+        gridDebugArray = new int[width, height];
 
-        // Draw cells
-        for(int x = 0; x < width; x++)
+        for (int x = 0; x < width; x++)
         {
-            for(int y = 0; y < height; y++)
+            for (int y = 0; y < height; y++)
             {
-                // Draw cells
-                debugTextArray[x,y] = CreateTextObject(gridArray[x, y].ToString(), CellToWorldPos(x, y));
-                Debug.DrawLine(CellToWorldPos(x, y) - new Vector3(cellSize, cellSize) * 0.5f, CellToWorldPos(x + 1, y) - new Vector3(cellSize, cellSize) * 0.5f, Color.white, 100f);
-                Debug.DrawLine(CellToWorldPos(x, y) - new Vector3(cellSize, cellSize) * 0.5f, CellToWorldPos(x, y + 1) - new Vector3(cellSize, cellSize) * 0.5f, Color.white, 100f);
+                // Create grid objects
+                gridArray[x, y] = createGridObject(this, x, y);
             }
         }
 
-        Debug.DrawLine(CellToWorldPos(0, height) - new Vector3(cellSize, cellSize) * 0.5f, CellToWorldPos(width, height) - new Vector3(cellSize, cellSize) * 0.5f, Color.white, 100f);
-        Debug.DrawLine(CellToWorldPos(width, 0) - new Vector3(cellSize, cellSize) * 0.5f, CellToWorldPos(width, height) - new Vector3(cellSize, cellSize) * 0.5f, Color.white, 100f);
+        if (true) ShowDebug();
     }
 
     private Vector3 CellToWorldPos(int x, int y)
     {
-        return new Vector3(x, y) * cellSize;
+        return new Vector3(x, y) * cellSize + startPoint;
     }
 
     public void WorldToCellPos(Vector3 worldPos, out int x, out int y)
     {
-        x = Mathf.FloorToInt(worldPos.x + cellSize * 0.5f / cellSize);
-        y = Mathf.FloorToInt(worldPos.y + cellSize * 0.5f / cellSize);
+        x = Mathf.FloorToInt((worldPos.x - startPoint.x + cellSize * 0.5f) / cellSize);
+        y = Mathf.FloorToInt((worldPos.y - startPoint.y + cellSize * 0.5f) / cellSize);
     }
 
     private TextMesh CreateTextObject(string text, Vector3 position, int fontSize = 100)
@@ -66,23 +79,40 @@ public class Grid
         if (!(x >= 0 && y >= 0 && x < width && y < height))
             return;
 
-        gridArray[x, y] = value;
-        debugTextArray[x, y].text = gridArray[x,y].ToString();
+        //gridDebugArray[x, y] = value;
+        //debugTextArray[x, y].text = gridArray[x,y].ToString();
     }
-    public void ToggleValue(int x, int y)
+    public void UpdateValue(int x, int y)
     {
-        if (!(x >= 0 && y >= 0 && x < width && y < height))
-            return;
-
-        if (gridArray[x, y] == 1)
-        {
-            gridArray[x, y] = 0;
-        }
-        else if (gridArray[x, y] == 0)
-        {
-            gridArray[x, y] = 1;
-        }
-
         debugTextArray[x, y].text = gridArray[x, y].ToString();
+    }
+
+    public GridObject GetGridObject(int x, int y)
+    {
+        return gridArray[x, y];
+    }
+
+    public GridObject GetGridObject(Vector3 pos)
+    {
+        WorldToCellPos(pos, out int x, out int y);
+        return GetGridObject(x, y);
+    }
+
+    private void ShowDebug()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+
+                // Draw cells
+                debugTextArray[x, y] = CreateTextObject(gridArray[x, y].ToString(), CellToWorldPos(x, y));
+                Debug.DrawLine(CellToWorldPos(x, y) - new Vector3(cellSize, cellSize) * 0.5f, CellToWorldPos(x + 1, y) - new Vector3(cellSize, cellSize) * 0.5f, Color.white, 100f);
+                Debug.DrawLine(CellToWorldPos(x, y) - new Vector3(cellSize, cellSize) * 0.5f, CellToWorldPos(x, y + 1) - new Vector3(cellSize, cellSize) * 0.5f, Color.white, 100f);
+            }
+        }
+
+        Debug.DrawLine(CellToWorldPos(0, height) - new Vector3(cellSize, cellSize) * 0.5f, CellToWorldPos(width, height) - new Vector3(cellSize, cellSize) * 0.5f, Color.white, 100f);
+        Debug.DrawLine(CellToWorldPos(width, 0) - new Vector3(cellSize, cellSize) * 0.5f, CellToWorldPos(width, height) - new Vector3(cellSize, cellSize) * 0.5f, Color.white, 100f);
     }
 }
--- Herald-Crypt/Assets/Scripts/TESTING.cs
+++ Herald-Crypt/Assets/Scripts/TESTING.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 
 public class TESTING : MonoBehaviour
 {
-    private Grid grid;
-
-    public bool debugOn;
+    private Pathfinding pathFinding;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (!debugOn) return;
-
-        grid = new Grid(10, 10, 1);
+        pathFinding = new Pathfinding(10, 10, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!debugOn) return;
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pathFinding.NodeGrid.WorldToCellPos(clickPos, out int x, out int y);
+            List<PathNode> path = pathFinding.FindPath(0, 0, x, y);
+
+            if(path != null)
+            {
+                for(int i = 0; i < path.Count - 1; i++)
+                {
+                    Debug.DrawLine(new Vector3(path[i].x, path[i].y), new Vector3(path[i + 1].x, path[i + 1].y), Color.green, 100f);
+                }
+            }
+        }
 
         if(Input.GetMouseButtonDown(1))
         {
             Vector3 clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            grid.WorldToCellPos(clickPos, out int x, out int y);
-            grid.ToggleValue(x, y);
+            PathNode currentNode = pathFinding.NodeGrid.GetGridObject(clickPos);
+            currentNode.ToggleWalkable();
+            pathFinding.NodeGrid.UpdateValue(currentNode.x, currentNode.y);
         }
     }
 }
