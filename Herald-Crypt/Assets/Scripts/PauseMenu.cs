3dd68b5572f1afe5de00e44ba7fa935b98636843
Anoop Rajeevan <44277457+anoopix@users.noreply.github.com>
2021-04-15 22:13:35 -0400

    Script for pause menu (press Esc)

---
+++ Herald-Crypt/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+    public GameObject pauseMenuUI;
+
+    public Button resumeBtn;
+    public Button quitBtn;
+
+    void Start(){
+        resumeBtn.onClick.AddListener(Resume);
+        quitBtn.onClick.AddListener(quitClick);
+    }
+
+    public void quitClick(){
+        Resume();
+        SceneManager.LoadScene("StartScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetKeyDown(KeyCode.Escape)){
+            if(gamePaused){
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log("Space pressed");
+        }
+    }
+
+    public void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+}
